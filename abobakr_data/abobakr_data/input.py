03d142166e262a3c628c927c7fe197231d8463ba
Abobakr-number-one-AI <abobakrmomtaz2011@gmail.com>
2026-08-18 15:25:05 +0300

    Rename input functions and refactor code

--- abobakr_data/abobakr_data/input.py
+++ abobakr_data/abobakr_data/input.py
@@ -1,5 +1,4 @@
-
-def input_float(message:str) -> float:
+def get_float(message: str) -> float:
     while True:
         try:
             number = float(input(message))
@@ -8,37 +7,46 @@ def input_float(message:str) -> float:
             continue
 
 
-def input_int(message:str) -> int:
+def get_int(message: str) -> int:
     while True:
         try:
             number = int(input(message))
             return number
         except ValueError:
             continue
-            
-def input_string(message:str) -> str:
+
+
+def get_string(message: str) -> str:
     input_string = input(message)
     while input_string.strip() == "" or input_string.isdigit():
         input_string = input(message)
     return input_string
 
-def print_line(number:int) -> None:
+
+def print_line(number: int) -> None:
     print("=" * number)
 
-def tuple_python(list_tuple):
+
+def convert_to_tuple(list_tuple):
     new_list = ()
     for x in list_tuple:
         new_list += (x,)
     return new_list
 
 
-def zip_python_list_list(list_zip):
-    smallest= len(list_zip[0])
+def convert_to_set(list_set):
+    new_set = ()
+    for x in list_set:
+        new_set += (x,)
+
+
+def zip_list_tuple(list_zip):
+    smallest = len(list_zip[0])
     for x in list_zip:
         if len(x) < smallest:
             smallest = len(x)
 
-    new_list = [ (n[i] for n in list_zip) for i in range(smallest)]
+    new_list = [(n[i] for n in list_zip) for i in range(smallest)]
     return new_list
 
 
